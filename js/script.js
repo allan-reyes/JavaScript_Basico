72922939e4544d0f3e33cd08b23c9c3a9da3a75a
allan-reyes <63377574+allan-reyes@users.noreply.github.com>
2021-01-18 10:33:55 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
+var saludo = "Hola JavaScript"
+var edad = 18
+var unPoco = 1.5
+var siNo = true
+var seccion = 'a'
+
+//MOSTRAR UN DATOS DE CONSOLA
+console.log(saludo + " " + edad)
+
+var datoApellido = document.getElementById('favorito')
